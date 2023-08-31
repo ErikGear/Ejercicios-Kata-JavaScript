853231ccb60d887f0e4a7f7bf42c52bc8fb62f02
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-31 17:56:32 -0600

    Agrgando el criterio de divisibilidad por 2

--- Ejercicio2-Condicionales/app.js
+++ Ejercicio2-Condicionales/app.js
@@ -1,9 +1,19 @@
-// 2. Divisibilidad de un numero
+// 2. Divisibilidad de un numero por 2
 
-const number = parseInt(prompt("Ingrse un numero"));
+const number1 = prompt("Ingrese un numero");
+const parseo = parseInt(number1);
+const digitos = number1.split(""); 
 
-if (number % 2 === 0) {
-    alert(`El numero ${number} es divisible por 2`);
+
+
+
+if (parseo >=1 && parseo <= 9) {
+    const mensaje = (parseo % 2 === 0) ? `El numero ${parseo} es divisible por 2`: `El numero ${parseo} no es divisible por 2`;
+    alert(mensaje)
+} else if (parseo >= 10) {
+    const ultimaCifra = parseInt(digitos[digitos.length - 1]);
+    const mensaje = (ultimaCifra % 2 === 0) ? `El numero ${parseo} es divisible por 2`: `El numero ${parseo} no es divisible por 2`;
+    alert(mensaje)
 } else {
-    alert(`El numero ${number} no es divisible por 2`);
+    alert (`Ingreso ${number1} debe digitar un numero valido`);
 }
