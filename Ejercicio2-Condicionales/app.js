b6a9e3bc55e9d5d307c4ff16e8ff691b42cfbdc5
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-31 16:57:34 -0600

    Agregando ejercicio 2

---
+++ Ejercicio2-Condicionales/app.js
@@ -0,0 +1,9 @@
+// 2. Divisibilidad de un numero
+
+const number = parseInt(prompt("Ingrse un numero"));
+
+if (number % 2 === 0) {
+    alert(`El numero ${number} es divisible por 2`);
+} else {
+    alert(`El numero ${number} no es divisible por 2`);
+}
