34f1ae5cedf78e067cc0b59bd425e1a49ec04c7e
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-31 17:02:02 -0600

    Ejercicio 3

---
+++ Ejercicio3-Condicionales/app.js
@@ -0,0 +1,9 @@
+// 3. numero par o impar
+
+const number = parseInt(prompt("Ingrse un numero"));
+
+if (number % 2 === 0) {
+    alert(`El numero ${number} es PAR`);
+} else {
+    alert(`El numero ${number} no es PAR`);
+}
