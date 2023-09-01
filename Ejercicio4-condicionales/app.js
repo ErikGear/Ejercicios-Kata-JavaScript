f995bcd0f7c030230c0dd671bbf58bf7d57531aa
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-31 18:00:18 -0600

    Ejercicio 4

---
+++ Ejercicio4-condicionales/app.js
@@ -0,0 +1,8 @@
+
+const numeroGanador = parseInt(prompt("Digite un numero"));
+
+if (numeroGanador === 1000) {
+    alert(`El numero es ${numeroGanador} Ganaste un premio`);
+} else {
+ alert(`El numero es ${numeroGanador} Lo sentimos siga participando`);   
+}
